b55dc4d7cad22a6dd16dded740bc53759d0a2005
eru27 <anja.ekres@gmail.com>
2022-08-11 04:37:53 +0200

    finished game of life

---
+++ GoL/game.py
@@ -0,0 +1,45 @@
+import inout as io
+from copy import deepcopy
+
+START = "start.txt"
+
+MATRIX_SIZE_1 = 43
+MATRIX_SIZE_2 = 80
+
+i_ADD = [-1, -1, -1, 0, 0, 1, 1, 1]
+j_ADD = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+def main():
+    global START
+    global MATRIX_SIZE_1
+    global MATRIX_SIZE_2
+    global i_ADD
+    global j_ADD
+
+    board = io.load(START, MATRIX_SIZE_1, MATRIX_SIZE_2) # Dodatni redovi za hendlovanje izuzetaka
+    board1 = [[0 for j in range(MATRIX_SIZE_2 + 2)] for i in range(MATRIX_SIZE_1 + 2)] # Dodatni redovi za hendlovanje izuzetaka
+
+    io.printBoard(board, MATRIX_SIZE_1, MATRIX_SIZE_2)
+    inp = input()
+
+    while not inp == '0':
+        for i in range(1, MATRIX_SIZE_1 + 1):
+            for j in range(1, MATRIX_SIZE_2 + 1):
+                liveCells = 0
+                for k in range(8):
+                    liveCells += board[i + i_ADD[k]][j + j_ADD[k]]
+
+                if liveCells <= 1 or liveCells >= 4:
+                    board1[i][j] = 0
+                elif board[i][j] == 1 and liveCells == 2 or liveCells == 3:
+                    board1[i][j] = 1
+                elif liveCells == 3:
+                    board1[i][j] = 1
+                else:
+                    board1[i][j] = 0
+        
+        board = deepcopy(board1)
+        io.printBoard(board, MATRIX_SIZE_1, MATRIX_SIZE_2)
+        inp = input()
+
+main()
---
+++ GoL/inout.py
@@ -0,0 +1,29 @@
+from termcolor import colored
+import os
+
+def load(addrs, mats1, mats2):
+    with open(addrs, 'r') as file:
+        matrix = file.readlines()
+    matrix1 = []
+
+    for line in matrix:
+        matrix1.append(line.split())
+
+    matrix2 = [[0 for j in range(mats2 + 2)] for i in range(mats1 + 2)]
+    for i in range(0, mats1):
+        for j in range(0, mats2):
+            if matrix1[i][j] != '.':
+                matrix2[i + 1][j + 1] = 1
+
+    return matrix2
+
+def printBoard(board, mats1, mats2):
+    os.system('color')
+
+    for i in range(1, mats1 + 1):
+        for j in range(1, mats2 + 1):
+            if board[i][j]:
+                print(colored(u'\u25A0', 'yellow'), end = ' ') # BLACK SQUARE in the Geometric Shapes Unicode block.
+            else:
+                print(colored(u'\u25A0', 'grey'), end = ' ') # BLACK SQUARE in the Geometric Shapes Unicode block.
+        print()
